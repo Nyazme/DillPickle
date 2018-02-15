03b2e359eddd322268b45bba6e64035aa9434ebb
Mark Feinman <feinmanmarke@gmail.com>
2018-02-15 16:34:29 -0700

    2nd revision

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,16 @@
-public class Main {
+import static spark.Spark.*
 
+public class Main {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        get("/", ((request, response) -> {
+            response.redirect("/index.html");
+            return null;
+        }));
+        get("/people",((request, response) -> {
+            System.out.println("KosherKrunch");
+            response.type("application/json");
+            return"{\n" +
+        }))
+    }
 }
