4a253714845c8355f037f8810b5f5b904fbff89f
Mark Feinman <feinmanmarke@gmail.com>
2018-02-16 15:34:31 -0700

    complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import static spark.Spark.*
+import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,10 +7,57 @@ public class Main {
             response.redirect("/index.html");
             return null;
         }));
-        get("/people",((request, response) -> {
-            System.out.println("KosherKrunch");
+        get("/hotdog" ,((request, response) -> {
+            //System.out.println("blurbs");
             response.type("application/json");
-            return"{\n" +
-        }))
+            return "{\n" +
+                    "  \"blurbs\": [\n" +
+                    "    {\n" +
+                    "      \"name\": \"Scott\",\n" +
+                    "      \"mention\": \"I like how you explain code.\"\n" +
+                    "    },\n" +
+                    "    {\n" +
+                    "      \"name\":\"Koddie\",\n" +
+                    "      \"mention\":\"Thanks to Koddie for the new snacks!\"\n" +
+                    "    },\n" +
+                    "\n" +
+                    "    {\n" +
+                    "      \"name\": \"Mark\",\n" +
+                    "      \"mention\":  \"Shout out to Mark!  He's a great partner to work with. \"\n" +
+                    "    },\n" +
+                    "    {\n" +
+                    "      \"name\": \"Ted\",\n" +
+                    "      \"mention\": \"Ted helped me to better understand my weekend challenge.\"\n" +
+                    "    },\n" +
+                    "    {\n" +
+                    "      \"name\": \"Joan\",\n" +
+                    "      \"mention\": \"Joan helped me better understand the mini challenge.\"\n" +
+                    "    },\n" +
+                    "\n" +
+                    "    {\n" +
+                    "      \"name\": \"Manny\",\n" +
+                    "      \"mention\": \"Manny his patience and explanations helped me process the peer challenge.\"\n" +
+                    "    },\n" +
+                    "\n" +
+                    "    {\n" +
+                    "      \"name\": \"Lori\",\n" +
+                    "      \"mention\": \"I think Lori is thinking like a programmer and asking great questions.\"\n" +
+                    "    },\n" +
+                    "    {\n" +
+                    "      \"name\": \"Zech\",\n" +
+                    "      \"mention\": \"Zech walked me through each step and explained things that were confusing to me.\"\n" +
+                    "    },\n" +
+                    "    {\n" +
+                    "      \"name\": \"Kym\",\n" +
+                    "      \"mention\": \"Kym was helpful and patient.\"\n" +
+                    "    },\n" +
+                    "    {\n" +
+                    "      \"name\": \"Jake\",\n" +
+                    "      \"mention\": \"Shout out to Jake for working out our group challenge on the timer project.\"\n" +
+                    "    }\n" +
+                    "\n" +
+                    "  ]\n" +
+                    "}";
+        }));
     }
 }
